b9f03335485cab2f3d7b84ab6ea653e629d29f5e
skaianDestiny <33128418+skaianDestiny@users.noreply.github.com>
2021-06-17 00:48:53 -0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,15 +1,19 @@
 # Print the number of integers in an array that are above the given input and the number that are below, e.g. for the array [1, 5, 2, 1, 10] with input 6, print “above: 1, below: 4”.
 
 array = [1, 2, 5, 6, 8]
-num = input("Enter number to compare: ")
 
-below = 0
-above = 0
+try:
+	num = int(input("Enter number to compare: "))
+	
+	below = 0
+	above = 0
 
-for x in array:
-	if x > num:
-		above += 1
-	elif x < num:
-		below += 1
+	for x in array:
+		if x > num:
+			above += 1
+		elif x < num:
+			below += 1
 
-print("Above: " + str(above) + "\nBelow: " + str(below))
+	print("Above: " + str(above) + "\nBelow: " + str(below))
+except:
+	print("Not a valid number")
